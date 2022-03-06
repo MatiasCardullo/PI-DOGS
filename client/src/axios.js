a787e5395765a4b5d0a786185130fa7e4fda7089
MatiasCardullo <supermaty97@gmail.com>
2022-03-06 16:15:13 -0300

    Change URL

--- client/src/axios.js
+++ client/src/axios.js
@@ -4,7 +4,7 @@ dotenv.config();
 const {URLHOST}=process.env
 
 const instance = axios.create({
-    baseURL: URLHOST|| "http://localhost:3001"
+    baseURL: URLHOST
 });
 
 export default instance;
--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const routes = require('./routes/index.js');
 require('./db.js');
 const server = express();
 server.name = 'API';
+const {CORS_URL} =process.env
 const cors = require('cors')
 
 server.use(cors())
@@ -15,7 +16,7 @@ server.use(cookieParser());
 server.use(morgan('dev'));
 
 server.use((req, res, next) => {
-	res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+	res.header('Access-Control-Allow-Origin', CORS_URL); // update to match the domain you will make the request from
 	res.header('Access-Control-Allow-Credentials', 'true');
 	res.header(
 		'Access-Control-Allow-Headers',
