9b4df21cc943db6857a74a970d1446713f3c5d81
MatiasCardullo <36178170+MatiasCardullo@users.noreply.github.com>
2022-08-10 15:16:36 -0300

    only back

--- client/src/axios.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-import dotenv from 'dotenv';
-dotenv.config();
-
-const instance = axios.create({
-    baseURL: "http://localhost:3001"
-});
-
-export default instance;
--- client/src/containers/BreedDetail/StyledBreedDetail.jsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const StyledBreedDetail = styled.div`
-
-padding-top: 100px;
-    .img{
-        width: 300px
-    }
-`
